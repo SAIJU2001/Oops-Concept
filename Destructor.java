d4cb8a3175dc316d566c78011fef412ed9fa3f9b
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-22 00:44:17 +0530

    Add files via upload

---
+++ Destructor.java
@@ -0,0 +1,7 @@
+/*
+ * We cound not write any Destructor.
+ * java should have own gurbage collector.
+ * when any method or class unused in the program then the method or class goes to the     gurbage collector.
+ * Destructor is opposite of constructor to make the balance
+ * Destructor or gurbage collector works automatically that's why java is very efficient 
+ */
