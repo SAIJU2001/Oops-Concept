8bac8d1b7c30041a2eb235b86f5b1c51da31d8b9
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-24 09:29:28 +0530

    Add files via upload

---
+++ NonParameterizedConstructor.java
@@ -0,0 +1,44 @@
+// oops concept
+
+
+// this is the class or blue print
+class Animal 
+{
+    //properties of the class
+    String color;
+    String name;
+
+    //function of the class
+     public void print()
+     {
+         System.out.println("This is the function of class");
+         System.out.println("The color of the animal : "+this.color);
+         System.out.println("The type of the animal : "+this.name);
+         System.out.println();
+     }
+
+    //non parameterzied constructor
+    Animal()   //don't have any return type
+    {
+        System.out.println("non parameterzied constructor called.");
+    }
+}
+
+public class NonParameterizedConstructor
+{
+    public static void main(String args[])
+    {
+        //creat the object of Animal class
+        Animal A1 = new Animal();
+        A1.color="brown";
+        A1.name="cow";
+        A1.print();
+
+        Animal A2 = new Animal();
+        A2.color="black";
+        A2.name="dog";
+        
+        A1.print();
+        A2.print();
+    }
+}
